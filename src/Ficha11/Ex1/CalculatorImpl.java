f8485fe95757a277c2bac295447e9998d44d3d4a
JadirJunior <jadirjunior8@gmail.com>
2024-11-14 10:15:17 +0000

    Finishing Ficha11 with java RMI

--- src/Ficha11/Ex1/CalculatorImpl.java
+++ src/Ficha11/Ex1/CalculatorImpl.java
@@ -2,7 +2,17 @@ package Ficha11.Ex1;
 
 import Ficha11.Numbers;
 
-public class CalculatorImpl implements Calculator{
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CalculatorImpl extends UnicastRemoteObject implements Calculator{
+    protected CalculatorImpl() throws RemoteException {
+        super();
+    }
+
     @Override
     public float add(Numbers numbers) {
         return numbers.a + numbers.b;
@@ -21,5 +31,18 @@ public class CalculatorImpl implements Calculator{
     @Override
     public float div(Numbers numbers) {
         return numbers.b == 0 ? Float.NaN : (numbers.a/numbers.b);
+    }
+
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(6666);
+            CalculatorImpl calculator = new CalculatorImpl();
+            Naming.rebind("rmi://localhost:6666/calculator", calculator);
+            System.out.println("Calculadora preparada");
+        } catch (RemoteException e) {
+            System.err.println("Erro ao tentar exportar o objeto");
+        } catch (MalformedURLException e) {
+            System.err.println("URL mal definido");
+        }
     }
 }
--- src/Ficha11/Ex2/CalculatorClient.java
+++ src/Ficha11/Ex2/CalculatorClient.java
@@ -1,8 +1,76 @@
 package Ficha11.Ex2;
 
+import Ficha11.Ex1.Calculator;
+import Ficha11.Numbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+
 public class CalculatorClient {
 
     public static void main(String[] args) {
+
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+                ) {
+            Calculator calculador = (Calculator) Naming.lookup("rmi://localhost:6666/calculator");
+
+            while (true) {
+                System.out.println("Introduza \"Sair\" para terminar, ou a operação de cálculo efetuar: " +
+                        "<operando1> <operador> <operando2>");
+
+                String s = br.readLine();
+
+                if (s.equalsIgnoreCase("sair")) {
+                    break;
+                }
+
+                String[] pars = s.split(" ");
+
+                if (pars.length != 3) {
+                    System.out.println("Sintaxe inválida");
+                    continue;
+                }
+
+                Numbers numbers = new Numbers();
+                char op;
+
+                try {
+                    numbers.a = Float.parseFloat(pars[0]);
+                    numbers.b = Float.parseFloat(pars[2]);
+                    op = pars[1].charAt(0);
+                } catch (NumberFormatException e) {
+                    System.out.println("Sintaxe inváida");
+                    continue;
+                }
+
+                switch (pars[1]) {
+                    case "+":
+                        System.out.println(s + " = " + calculador.add(numbers));
+                        break;
+                    case "-":
+                        System.out.println(s + " = " + calculador.sub(numbers));
+                        break;
+                    case "*", "x", "X":
+                        System.out.println(s + " = " + calculador.multi(numbers));
+                        break;
+                    case "/", ":":
+                        System.out.println(s + " = " + calculador.div(numbers));
+                        break;
+                    default:
+                        System.out.println("Operador inválido");
+                        break;
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Ocorreu um erro ao tentar ler o texto introduzido pelo utilizador");
+        } catch (NotBoundException e) {
+            System.err.println("Erro ao tentar encontrar o servidor rmi");
+        }
 
     }
 
